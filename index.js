15692f97c8bc2409b4db8fe404f40dcda0c1c346
yashvi <you@example.com>
2023-10-12 09:38:08 +0530

    nodemailer

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const nodemailer = require("nodemailer");
+app.use(express.json());
+
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "yashvivastarpara@gmail.com",
+    pass: "wskynsoshfzwmagy",
+  },
+});
+
+
+let Otp = Math.floor(Math.random() * 10000);
+
+app.get("/:otp", (req, res) => {
+  let {otp} = req.params;
+  if (Otp == otp) {
+    console.log("otp successfully");
+    res.send('otp successfully')
+  } else {
+    console.log("otp failed");
+    res.send('otp failed')
+  }
+ 
+});
+
+app.post("/", (req, res) => {
+  
+  const mailOptions = {
+    from: "yashvivastarpara@gmail.com",
+    to: req.body.email,
+    subject: req.body.subject,
+    html: `<h1>${Otp}</h1>`,
+  };
+
+  
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(info);
+    }
+  });
+  res.send("success in data");
+});
+
+app.listen(8090, () => {
+  console.log("listining port 8090");
+});
